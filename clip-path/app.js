b726a1778138142f7077e55d9c23e34d5518b37d
Dani <danielmateu86@gmail.com>
2022-01-18 12:40:15 +0100

    clip path done

--- clip-path/app.js
+++ clip-path/app.js
@@ -1,2 +1,6 @@
 'use strict'
 
+const menu = document.getElementById('btn-abrir-menu');
+menu.addEventListener('click', ()=>{
+    document.getElementById('contenedor-menu').classList.toggle('active');
+})
