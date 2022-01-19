0e91c6eaf05a03a2874ed9484e695967a1f2ca67
Dani <danielmateu86@gmail.com>
2022-01-19 16:04:34 +0100

    app done

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 'use strict'
-
+const menu = document.getElementById('btn-abrir-menu');
+menu.addEventListener('click',()=>{
+    document.getElementById('container-menu').classList.toggle('active');
+});
 //Links de interés -> https://soundcloud.com/danielmateupardo
 
